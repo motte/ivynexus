eb699ae9d7a518f136d1df79c2a99fa48bdc9892
motte <michael.l.otte.13@dartmouth.edu>
2012-12-31 20:40:18 -0500

    Reverted back to all mysql

--- controllers/ideas/loadcontroller.php
+++ controllers/ideas/loadcontroller.php
@@ -3,15 +3,15 @@
 
 
 	include_once('../../dbconnect.php');
-	$counter = mysqli_real_escape_string($_GET["c"]); //initial load has counter =0
-	$first = mysqli_real_escape_string($_GET["d"]); //starts from 0, first time loading a set of posts, then adds minimum row value
+	$counter = mysql_real_escape_string($_GET["c"]); //initial load has counter =0
+	$first = mysql_real_escape_string($_GET["d"]); //starts from 0, first time loading a set of posts, then adds minimum row value
 	
 //If not the first post
 	if($first != '0') {
-		$query = mysqli_query("SELECT * FROM ideas WHERE id BETWEEN $counter-26 AND $counter-1 ORDER BY id DESC LIMIT 25");
+		$query = mysql_query("SELECT * FROM ideas WHERE id BETWEEN $counter-26 AND $counter-1 ORDER BY id DESC LIMIT 25");
 
 		$min = array();
-		while($row = mysqli_fetch_array($query)){
+		while($row = mysql_fetch_array($query)){
 			$redirect = "'ideas/view/".$row["id"]."'";
 		    	if($row['poster_name'] == '') {
 				echo '<tr height="40px" class="threadlist" onclick="document.location='.$redirect.'" valign="top"><td style="border-bottom: 1px solid #fff;"><span id="chilirefresh'.$row['id'].'">'.$row['chilis'].'</span><br /><form id="ivychili" name="form" method="post"><input id="ivychili" title="Hotness Factor: '.$row['chilis'].' Chilis" type="button" onclick="addChili('.$row['id'].')" /></form></td><td  style="border-bottom: 1px solid #fff;"><a href="ideas/view/'.$row["id"].'" class="threadlist">'.$row["idea_name"].'<br />'.$row["description"].'</a></td><td  style="border-bottom: 1px solid #fff;"><a href="profile/view/'.$row["poster_id"].'">'.$row["poster_name"].'</a></td><td  style="border-bottom: 1px solid #fff;">'.$row["when_added"].'</td></tr>';
@@ -25,7 +25,7 @@
 	}
 // if the first set of posts - mark it
 	else {
-		$query = mysqli_query("SELECT * FROM ideas ORDER BY id DESC LIMIT 25");
+		$query = mysql_query("SELECT * FROM ideas ORDER BY id DESC LIMIT 25");
 		
 		$min = array();
 		
