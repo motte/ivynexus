cbd1fa4b75031fd4cadaddc41140a62ab5d1ffb5
motte <michael.l.otte.13@dartmouth.edu>
2013-08-04 12:34:56 -0400

    Update dbconnecti.php

--- dbconnecti.php
+++ dbconnecti.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('localhost', 'root', 'G3n3212R3y32', 'ivynex5_nexus') or die('error');
+$link = mysqli_connect('localhost', 'root', 'Passwrd', 'ivynex5_nexus') or die('error');
 
 if (mysqli_connect_error()) {
 	$logMessage = 'MySQL Error: ' . mysqli_connect_error();
